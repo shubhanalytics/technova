25e49f35e14042facb71671da7ab646d8c5db8c5
shubhanalytics <shubhank.katarey@outlook.com>
2026-03-02 00:18:15 +0530

    Remove test for deleted security_checks.py

--- tests/test_security_checks.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-def test_security_checks():
-    res = subprocess.run([sys.executable, 'scripts/security_checks.py'], capture_output=True, text=True)
-    print(res.stdout)
-    assert res.returncode == 0
